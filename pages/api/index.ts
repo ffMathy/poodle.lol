00c98cf74e9234248136ea8fc2b898dea7c228b4
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2023-05-12 20:30:38 +0200

    Progress on move.

---
+++ pages/api/index.ts
@@ -0,0 +1,2 @@
+import { appRouter } from './_src/app-router';
+export type { AppRouter } from './_src/app-router';
--- src/apps/backend/deploy.sh
+++
@@ -1,3 +0,0 @@
-cd ./dist
-zip -r ./function.zip .
-aws lambda update-function-code --function-name poodle-lol --zip-file fileb://function.zip
