c45247ac5390e305cd09ed0147ba3826a0ea5cdd
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2023-05-13 22:59:23 +0200

    Fix for compile issues.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,18 @@
-import { component$, useComputed$, useSignal, $, Slot } from '@builder.io/qwik';
-import { DocumentHead, Form, routeAction$, z, zod$ } from '@builder.io/qwik-city';
+import { component$, useComputed$, useSignal, $, Slot, useTask$, useVisibleTask$ } from '@builder.io/qwik';
+import { DocumentHead, Form, globalAction$, routeAction$, z, zod$ } from '@builder.io/qwik-city';
 import DatePicker from '~/components/date-picker';
 import Checkbox from '~/components/checkbox';
 import Combobox from '~/components/combobox';
 import { TimePerDayPicker } from '~/components/time-picker';
 import { setHours, setMinutes } from 'date-fns';
+import { nanoid } from 'nanoid';
 
-export const useCreateAppointment = routeAction$(
+export const useCreateAppointment = globalAction$(
   async (data, requestEvent) => {
 
   },
   zod$({
-    creator: z.object({
-      id: z.string(),
-      name: z.string()
-    }),
+    creatorId: z.string(),
     title: z.string(),
     description: z.optional(z.string()),
     location: z.optional(z.string()),
@@ -24,28 +22,68 @@ export const useCreateAppointment = routeAction$(
     }))
   }));
 
+export const useCreateUser = globalAction$(
+  async (data, requestEvent) => {
+    return { id: nanoid() };
+  },
+  zod$(z.void()));
+
+const useUserId = () => {
+  const createUser = useCreateUser();
+
+  const userId = useSignal<string>();
+
+  useVisibleTask$(async () => {
+    const currentUserId = localStorage.getItem("user-id");
+    if (currentUserId) {
+      userId.value = currentUserId;
+      return;
+    }
+
+    const createUserResponse = await createUser.submit();
+    const createdUserId = createUserResponse.value.id!;
+    userId.value = createdUserId;
+    localStorage.setItem("user-id", createdUserId)
+  });
+
+  return userId;
+}
+
 export default component$(() => {
   const createAppointment = useCreateAppointment();
+
   const selectedDates = useSignal(new Array<Date>());
+  const userId = useUserId();
 
   return <Form action={createAppointment}>
+    <input type="hidden" name="creatorId" value={userId.value} />
     <Section
       title="What"
       description="Describe what your event is about."
     >
       <div class="sm:col-span-4">
-        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
         <div class="mt-2">
           <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input type="text" autoComplete="name" class="block flex-1 border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="John's birthday party" />
+            <input 
+              type="text" 
+              name="title"
+              class="block flex-1 border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" 
+              placeholder="John's birthday party" />
           </div>
         </div>
       </div>
 
       <div class="col-span-full">
-        <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+        <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
         <div class="mt-2">
-          <textarea id="about" name="about" rows={3} class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Looking forward to seeing you at my birthday party! Remember the presents."></textarea>
+          <textarea 
+            id="description" 
+            name="description" 
+            rows={3} 
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+            placeholder="Looking forward to seeing you at my birthday party! Remember the presents." 
+          />
         </div>
       </div>
     </Section>
@@ -79,8 +117,8 @@ export default component$(() => {
     </Section>
 
     <Section>
-      <button 
-        type="submit" 
+      <button
+        type="submit"
         class="place-self-center w-64 col-span-full rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         Save
@@ -99,10 +137,10 @@ const Section = component$((props: {
   return <div class="space-y-10 divide-y divide-gray-900/10 mb-10">
     <div class="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
       <div class="px-4 sm:px-0">
-        {props.title && 
+        {props.title &&
           <h2 class="text-base font-semibold leading-7 text-gray-900">{props.title}</h2>}
 
-        {props.description && 
+        {props.description &&
           <p class="mt-1 text-sm leading-6 text-gray-600">{props.description}</p>}
       </div>
       <div class={`${classNameAdd} sm:rounded-xl md:col-span-2`}>
