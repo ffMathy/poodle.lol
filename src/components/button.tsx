a629a07b72e82d469df7e44bac5c6ab4f82e91da
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2023-05-19 17:28:28 +0200

    Accessibility.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,6 +6,7 @@ export default component$((props: {
     onClick$: () => void
 }) => {
     return <button 
+        aria-label={props.label}
         type="button" 
         onClick$={props.onClick$}
         class={`inline-flex items-center gap-x-1.5 rounded-md px-2.5 py-1.5 text-sm font-semibold shadow-sm bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${props.class ?? ""}`}
