e8d799895eba6a75347a4661f7bcc22c0b6db733
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2023-05-23 19:17:40 +0200

    Add analytics.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,6 +4,8 @@ import { routeLoader$ } from '@builder.io/qwik-city';
 import Header from '~/components/header';
 import Footer from '~/components/footer';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default component$(() => {
   return (
     <div class="page">
@@ -16,6 +18,7 @@ export default component$(() => {
           <Footer />
         </div>
       </div>
+      <Analytics />
     </div>
   );
 });
