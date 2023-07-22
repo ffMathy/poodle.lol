8395a200f2af249a690ae1ee1bedb630c23e90ee
Mathias Lorenzen <mathias.lorenzen@live.com>
2023-07-22 22:54:59 +0200

    Fix for duration field.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { component$, useSignal, $, useVisibleTask$ } from '@builder.io/qwik';
 import { DocumentHead, routeLoader$, useNavigate } from '@builder.io/qwik-city';
 import { InitialValues, SubmitHandler, getValue, getValues, insert, remove, replace, setValue, setValues, swap, useForm, zodForm$ } from '@modular-forms/qwik';
-import { getDurationsInMinutes, renderLabelFromValue as renderDurationLabelFromDurationInMinutes } from './duration-section';
+import { getDurationsInMinutes, renderLabelFromValue as getLabelFromDurationInMinutes } from './duration-section';
 import { DateSection } from './date-section';
 import { Section } from './section';
 import { AppointmentRequest, appointmentRequestSchema, defaultAppointmentRequest, useCreateAppointment, useCreateUser } from './backend';
@@ -61,7 +61,7 @@ export default component$(() => {
     navigate(`/${result.value.id!}`)
   });
 
-  return <Form onSubmit$={onFormSubmitted} shouldActive={false}>
+  return <>
     {process.env.NODE_ENV === "development" && <div class="mb-5">
       <button class="bg-red-400" onClick$={() => {
         const values = getValues(form);
@@ -70,110 +70,115 @@ export default component$(() => {
         Fetch form
       </button>
     </div>}
-    <Section
-      title="What"
-      description="Describe what your event is about."
-    >
-      <div class="sm:col-span-4">
-        <Field name="title">
-          {(field, props) =>
-            <TextInput
-              {...props}
-              type="text"
-              error={field.error}
-              value={field.value}
-              label="Title"
-              placeholder="John's birthday party" />}
-        </Field>
-      </div>
+    <Form onSubmit$={onFormSubmitted} shouldActive={false}>
 
-      <div class="col-span-full">
-        <Field name="description">
-          {(field, props) =>
-            <TextArea
-              {...props}
-              error={field.error}
-              value={field.value}
-              label="Description"
-              placeholder="Looking forward to seeing you at my birthday party! Remember the presents." />}
-        </Field>
-      </div>
-    </Section>
+      <Section
+        title="What"
+        description="Describe what your event is about."
+      >
+        <div class="sm:col-span-4">
+          <Field name="title">
+            {(field, props) =>
+              <TextInput
+                {...props}
+                type="text"
+                error={field.error}
+                value={field.value}
+                label="Title"
+                placeholder="John's birthday party" />}
+          </Field>
+        </div>
 
-    <Section
-      title="Where"
-      description="Describe where to meet."
-    >
-      <div class="col-span-full">
-        <Field name="location">
-          {(field, props) =>
-            <TextArea
-              {...props}
-              error={field.error}
-              value={field.value}
-              label="Location"
-              placeholder="Central Park" />}
-        </Field>
-      </div>
-    </Section>
+        <div class="col-span-full">
+          <Field name="description">
+            {(field, props) =>
+              <TextArea
+                {...props}
+                error={field.error}
+                value={field.value}
+                label="Description"
+                placeholder="Looking forward to seeing you at my birthday party! Remember the presents." />}
+          </Field>
+        </div>
+      </Section>
 
-    <Section
-      title="When"
-      description="Add the times that are valid for attendees to pick from."
-    >
-      <div class="sm:col-span-4">
-        <Select
-            form={form}
-            fieldPath="durationInMinutes"
-            options={getDurationsInMinutes().map(durationInMinutes => ({
-              label: renderDurationLabelFromDurationInMinutes(durationInMinutes),
-              value: durationInMinutes
-            }))}
-            label="Duration"
-          />
-      </div>
+      <Section
+        title="Where"
+        description="Describe where to meet."
+      >
+        <div class="col-span-full">
+          <Field name="location">
+            {(field, props) =>
+              <TextArea
+                {...props}
+                error={field.error}
+                value={field.value}
+                label="Location"
+                placeholder="Central Park" />}
+          </Field>
+        </div>
+      </Section>
 
-      <DateSection
-        key="date-section"
-        onAdded$={date => {
-          insert(form, "startTimesPerDay", {
-            value: {
-              day: date,
-              times: [date]
-            }
-          });
-        }}
-        onDeleted$={index => {
-          remove(
-            form,
-            "startTimesPerDay",
-            {
-              at: index
+      <Section
+        title="When"
+        description="Add the times that are valid for attendees to pick from."
+      >
+        <div class="sm:col-span-4">
+          <Field name="durationInMinutes" type="number">
+            {(field, props) => 
+              <Select
+                form={form}
+                fieldPath="durationInMinutes"
+                options={getDurationsInMinutes().map(durationInMinutes => ({
+                  label: getLabelFromDurationInMinutes(durationInMinutes),
+                  value: durationInMinutes
+                }))}
+                label="Duration"
+              />}
+          </Field>
+        </div>
+
+        <DateSection
+          key="date-section"
+          onAdded$={date => {
+            insert(form, "startTimesPerDay", {
+              value: {
+                day: date,
+                times: [date]
+              }
             });
-        }}
-      />
+          }}
+          onDeleted$={index => {
+            remove(
+              form,
+              "startTimesPerDay",
+              {
+                at: index
+              });
+          }}
+        />
 
-      <FieldArray name="startTimesPerDay">
-        {(startTimesPerDayArray) => <>
-          <InputError error={startTimesPerDayArray.error} />
+        <FieldArray name="startTimesPerDay">
+          {(startTimesPerDayArray) => <>
+            <InputError error={startTimesPerDayArray.error} />
 
-          {startTimesPerDayArray.items.map((item, dayIndex) => {
-            const day = getValue(form, `${startTimesPerDayArray.name}.${dayIndex}.day`, {
-              shouldActive: false
-            })!;
-            return <div key={item} class="col-span-full">
-              <label for="about" class="block text-sm font-light leading-6 text-gray-900 mb-2">
-                {format(day, "PPP")}
-              </label>
-              <div>
-                <FieldArray name={`${startTimesPerDayArray.name}.${dayIndex}.times`}>
-                  {(timeFieldArray) => <>
-                    {timeFieldArray.items.map((item, timeIndex) =>
-                      <div
-                        key={`time-${item}-${timeIndex}`}
-                        class="flex mb-2"
-                      >
-                        {/* <Field name={`${timeFieldArray.name}.${timeIndex}`}>
+            {startTimesPerDayArray.items.map((item, dayIndex) => {
+              const day = getValue(form, `${startTimesPerDayArray.name}.${dayIndex}.day`, {
+                shouldActive: false
+              })!;
+              return <div key={item} class="col-span-full">
+                <label for="about" class="block text-sm font-light leading-6 text-gray-900 mb-2">
+                  {format(day, "PPP")}
+                </label>
+                <div>
+                  <FieldArray name={`${startTimesPerDayArray.name}.${dayIndex}.times`}>
+                    {(timeFieldArray) => <>
+                      {timeFieldArray.items.map((item, timeIndex) =>
+                        <div
+                          key={`time-${item}-${timeIndex}`}
+                          class="flex mb-2"
+                        >
+                          {/* <Field name={`${timeFieldArray.name}.${timeIndex}`}>
                           {(field, props) => <TimePicker
                             fieldProps={props}
                             selectedTime={getValue(form, `${timeFieldArray.name}.${timeIndex}`, { shouldActive: false })}
@@ -183,53 +188,54 @@ export default component$(() => {
                             })}
                           />}
                         </Field> */}
-                        <button
-                          title="Remove time"
-                          hidden={timeFieldArray.items.length === 1}
-                          type="button"
-                          class="ml-1 rounded-full p-1 text-indigo-600 focus-visible:outline"
-                          onClick$={() => {
-                            remove(form, timeFieldArray.name, { at: timeIndex })
-                          }}
-                        >
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                          </svg>
-                        </button>
-                      </div>
-                    )}
-                  </>}
-                </FieldArray>
+                          <button
+                            title="Remove time"
+                            hidden={timeFieldArray.items.length === 1}
+                            type="button"
+                            class="ml-1 rounded-full p-1 text-indigo-600 focus-visible:outline"
+                            onClick$={() => {
+                              remove(form, timeFieldArray.name, { at: timeIndex })
+                            }}
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                          </button>
+                        </div>
+                      )}
+                    </>}
+                  </FieldArray>
 
-                <Button
-                  class="mt-1"
-                  label="Add time"
-                  onClick$={() => {
-                    insert(form, `${startTimesPerDayArray.name}.${dayIndex}.times`, {
-                      value: day
-                    });
-                  }}
-                >
-                  <svg q: slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
+                  <Button
+                    class="mt-1"
+                    label="Add time"
+                    onClick$={() => {
+                      insert(form, `${startTimesPerDayArray.name}.${dayIndex}.times`, {
+                        value: day
+                      });
+                    }}
+                  >
+                    <svg q: slot="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
 
-                </Button>
+                  </Button>
+                </div>
               </div>
-            </div>
-          })}
-        </>}
-      </FieldArray>
-    </Section>
+            })}
+          </>}
+        </FieldArray>
+      </Section>
 
-    <Section>
-      <button
-        type="submit"
-        class="place-self-center w-64 col-span-full rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Save
-      </button>
-    </Section>
-  </Form >
+      <Section>
+        <button
+          type="submit"
+          class="place-self-center w-64 col-span-full rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </Section>
+    </Form>
+  </>
 });
 
